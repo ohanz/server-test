226be8a30919da961e95875cd670b5926b1443f3
Ohanz <chiaguguadaniel@gmail.com>
2024-10-16 23:49:24 +0100

    little changes

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello, World!\n');
+//   res.end('Hello, World!\n');
+  res.end('Hye, Ohanz Here!\n');
 });
 
 server.listen(3000, () => {
